ab797f4c15908c588d09879c4f82ab58200ba834
Petrov Dumitru <petrov.dumitru@protonmail.com>
2019-11-13 20:50:47 +0000

    added EventDispatcher.

--- public/front.php
+++ public/front.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 require_once __DIR__."/../vendor/autoload.php";
 
 use Symfony\Component\HttpFoundation\Request;
@@ -8,6 +9,11 @@ use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
+$dispatcher->addSubscriber(new Simplex\AnalyticsListener());
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -18,7 +24,7 @@ $matcher = new UrlMatcher($routes, $context);
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 $response = $framework->handle($request);
 
 $response->send();
--- src/Simplex/framework.php
+++ src/Simplex/framework.php
@@ -1,6 +1,7 @@
 <?PHP
 namespace Simplex;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class Framework {
+	protected $dispatcher;
 	protected $matcher;
 	protected $controllerResolver;
 	protected $argumentResolver;
 
-	public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
+	public function __construct(EventDispatcher $dispatcher, UrlMatcherInterface $matcher, ControllerResolverInterface $controllerResolver, ArgumentResolverInterface $argumentResolver)
 	{
+		$this->dispatcher = $dispatcher;
 		$this->matcher = $matcher;
 		$this->controllerResolver = $controllerResolver;
 		$this->argumentResolver = $argumentResolver;
@@ -30,11 +33,16 @@ class Framework {
 			$controller = $this->controllerResolver->getController($request);
 			$arguments = $this->argumentResolver->getArguments($request, $controller);
 
-			return call_user_func_array($controller, $arguments);
+			$response = call_user_func_array($controller, $arguments);
 		} catch (ResourceNotFoundException $exception) {
-			return new Response('Not Found', 404);
+			$response = new Response('Not Found', 404);
 		} catch (\Exception $exception) {
-			return new Response('An error occurred', 500);
+			$response = new Response('An error occurred', 500);
 		}
+
+		// dispatch a response event
+		$this->dispatcher->dispatch('response', new ResponseEvent($request, $response));
+
+		return $response;
 	}
 }
